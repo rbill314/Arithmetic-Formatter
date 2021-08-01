7a03a8d75ff488b63073055062dcd3b7b4ee4f64
rbill314 <rbill314@gmail.com>
2021-08-01 02:52:06 -0400

    added commit

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -64,7 +64,9 @@ def arithmetic_arranger(problems, *args):
     # The video I watched that (which she was kind enough to take the time to provide)
     # is at her youTube channel Faraday Academy.. https://www.youtube.com/c/FaradayAcademy/featured
     # The link is https://www.youtube.com/watch?v=ZBJUihOVvVM
+
     # This is my first time with python and I will come back to this project as I
     # further skills through the other projects.
+    
     # 7/4/2021
     # Done on VS Code
